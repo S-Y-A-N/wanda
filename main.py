392d0a87bfa495f971f4166275051dcc3a250f1e
SYAN <syanays.git@gmail.com>
2026-01-10 19:45:11 +0300

    set max seqlen to 8192

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_llm(model_name, cache_dir="llm_weights"):
         device_map="auto"
     )
 
-    model.seqlen = model.config.max_position_embeddings
+    model.seqlen = min(model.config.max_position_embeddings, 8192)
     print('model.seqlen =', model.seqlen)
     return model
 
